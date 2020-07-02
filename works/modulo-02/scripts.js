67cc7f8b0a66f6fb1175907dec4e9e5f24a67443
Wagner Moreira <47096330+wagnermor@users.noreply.github.com>
2020-07-02 00:23:56 -0300

    "Comentário deletado pelo site github"

--- works/modulo-02/scripts.js
+++ works/modulo-02/scripts.js
@@ -1,6 +1,5 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card')
-// const closeModal = document.querySelector('.close-modal')
 
 for(let card of cards) {
     card.addEventListener("click", function() {
