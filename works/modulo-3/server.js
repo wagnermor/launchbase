654b39bc0a16eaf99338e8d40d3c5efc3f2129ff
Wagner Moreira <wagnermor@outlook.com>
2020-07-03 19:34:19 -0300

    Template engine concluído

--- works/modulo-3/server.js
+++ works/modulo-3/server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
-server.get("/", function(req, res) {
-    return res.send(`Hi man!\nHow are you?`)
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function (req, res) {
+    return res.render("about")
+})
+
+server.get("/portfolio", function (req, res) {
+    return res.render("portfolio")
 })
 
 server.listen(5000, function() {
-    console.log("server is runing")
+    console.log("server is runing man!")
 })
