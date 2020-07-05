3c998b87a8bbc72186b38a6a6bf91f5979b36b66
Wagner Moreira <wagnermor@outlook.com>
2020-07-04 23:44:54 -0300

    usando dados dinâmicos

---
+++ works/modulo-3/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "2alg7MQ6_sI",
+        title: "Trabalhanco com Git e Github na prática",
+        duration: "33min 43",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS GRID na prática",
+        duration: "36min 17",
+        price: "Free"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário animado com JS puro e CSS animation",
+        duration: "57min 56",
+        price: "Free"
+    },
+    {
+        id: "ghTrp1x_1As",
+        title: "O que é API REST e RESTful",
+        duration: "33min 57",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável THIS",
+        duration: "48min 21",
+        price: "Free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "ARRAY: High order functions",
+        duration: "54min 07",
+        price: "Free"
+    }
+]
