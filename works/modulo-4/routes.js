a5b41392a9a6868ebba0d9218a2b3ee8108889b2
Wagner Moreira <wagnermor@outlook.com>
2020-07-14 12:26:45 -0300

    Validando dados no back end

--- works/modulo-4/routes.js
+++ works/modulo-4/routes.js
@@ -14,6 +14,12 @@ routes.get('/instructors/create', function(req, res) {
 })
 
 routes.post("/instructors", function(req, res) {
+    const keys = Object.keys(req.body)
+
+    for(key of keys) {
+        if(req.body[key] =="")
+            return res.send('Por favor, preencha todos os campos!')
+    }
     return res.send(req.body)
 })
 
