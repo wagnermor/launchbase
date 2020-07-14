db4257484f34341ed1fbc8a41ab9bbfca43864b3
Wagner Moreira <wagnermor@outlook.com>
2020-07-14 13:39:06 -0300

    Usando node fs e entendendo o 'callback function'

--- works/modulo-4/instructors.js
+++ works/modulo-4/instructors.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 // create
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
@@ -7,5 +9,12 @@ exports.post = function(req, res) {
             return res.send('Por favor, preencha todos os campos!')
         }
     }
+
+    fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
+        if (err) return res.send("Write file error guys!")
+        
+        return res.redirect("/instructors")
+    })
+
     return res.send(req.body)
 }
