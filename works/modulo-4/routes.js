7274e09526398d262c3fbaf4311fc58c2d8f13c5
Wagner Moreira <wagnermor@outlook.com>
2020-07-14 00:49:35 -0300

    Recebendo dados do front-end com 'req.body'

--- works/modulo-4/routes.js
+++ works/modulo-4/routes.js
@@ -5,8 +5,16 @@ routes.get('/', function(req, res) {
     return res.redirect("/instructors")
 })
 
-routes.get('/instructors', function(req,res) {
+routes.get('/instructors', function(req, res) {
     return res.render("instructors/index")
+})
+
+routes.get('/instructors/create', function(req, res) {
+    return res.render("instructors/create")
+})
+
+routes.post("/instructors", function(req, res) {
+    return res.send(req.body)
 })
 
 routes.get('./members', function(req, res) {
