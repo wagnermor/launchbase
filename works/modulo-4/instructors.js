caabdec602281db26b8a219d3491ddf4bab322b2
Wagner Moreira <wagnermor@outlook.com>
2020-07-14 14:14:02 -0300

    Trabalhando com dados '*.json'

--- works/modulo-4/instructors.js
+++ works/modulo-4/instructors.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const data = require("./data.json")
 
 // create
 exports.post = function(req, res) {
@@ -10,11 +11,13 @@ exports.post = function(req, res) {
         }
     }
 
-    fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("Write file error guys!")
         
         return res.redirect("/instructors")
     })
 
-    return res.send(req.body)
+    // return res.send(req.body)
 }
